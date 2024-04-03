037efb6d434792994408b9496cae839949886e52
RuthT Sobande <136115703+RuthTSobande@users.noreply.github.com>
2024-04-03 19:21:34 +0100

    Update 3-square.py

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,22 +1,32 @@
 #!/usr/bin/python3
+"""Class Square defines a square"""
+
+
 class Square:
-    """
-    creates a square object
+    """Class defines a square.
+
+    Class has no public attributes.
+
     """
     def __init__(self, size=0):
-        if(type(size) is not int):
-            raise TypeError("size must be an integer")
-        if(size < 0):
-            raise ValueError("size must be >= 0")
-        self.__size = size
-        """
-        initializes instance of a square
+        """Method initiates a square.
+
         Args:
-            __size(int): size of square
-            __position(tuple):position
+            size (int): Defines the size of the square.
+                Size is validated with try/except.
+
         """
+        try:
+            self.__size = size
+            if size < 0:
+                raise ValueError
+            if type(size) is not int:
+                raise TypeError
+        except TypeError:
+            raise TypeError("size must be an integer")
+        except ValueError:
+            raise ValueError("size must be >= 0")
+
     def area(self):
-        return(self.__size**2)
-    """
-    returns area of square based on size
-    """
+        """int: Return area of square."""
+        return self.__size * self.__size
