ba3ac857a5ae4d9e46edc7b61ea9a24cf528affc
RuthT Sobande <136115703+RuthTSobande@users.noreply.github.com>
2024-04-08 20:04:20 +0100

    Update 101-nqueens.py

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -1,54 +1,72 @@
 #!/usr/bin/python3
-"""Standalone module to solve the nqueens problem"""
 
+"""
+N queens problem
+"""
 
 import sys
 
 
-def nqueens(size):
-    """Initial setup before recursive call"""
-    if type(size) is not int:
+class NQueens:
+    """ Class for solving the N Queens problem """
+
+    def __init__(self, n):
+        """ Initialize N Queens """
+        self.n = n
+        self.columns = []
+        self.diag1 = []
+        self.diag2 = []
+        self.solutions = []
+
+    def solve(self):
+        """ Solve the N Queens problem """
+        self._solve(0)
+        self._display()
+
+    def _solve(self, row):
+        """ Recursive function to solve N Queens """
+        if row == self.n:
+            solution = []
+            for i in range(self.n):
+                solution.append([i, self.columns[i]])
+            self.solutions.append(solution)
+        else:
+            for col in range(self.n):
+                if (
+                    col not in self.columns and
+                    row + col not in self.diag1 and
+                    row - col not in self.diag2
+                ):
+                    self.columns.append(col)
+                    self.diag1.append(row + col)
+                    self.diag2.append(row - col)
+                    self._solve(row + 1)
+                    self.columns.pop()
+                    self.diag1.pop()
+                    self.diag2.pop()
+
+    def _display(self):
+        """ Display the solutions """
+        for solution in self.solutions:
+            print(solution)
+
+
+if __name__ == "__main__":
+    # Check the command-line arguments
+    if len(sys.argv) != 2:
+        print("Usage: nqueens N")
+        sys.exit(1)
+
+    try:
+        n = int(sys.argv[1])
+    except ValueError:
         print("N must be a number")
-        return
-    if size < 4:
+        sys.exit(1)
+
+    if n < 4:
         print("N must be at least 4")
-        return
-    queens = [0] * size
+        sys.exit(1)
 
-    def printsolution(queens):
-        print("[[0, ", queens[0], "]", sep="", end="")
-        for y, x in enumerate(queens[1:], 1):
-            print(", [", y, ", ", x, "]", sep="", end="")
-        print("]")
-
-    def queencalc(queen):
-        """Recursive call queen position validator"""
-        for x in range(size):
-            """horizontal board positions per queen"""
-            nextx = 0
-            for y in range(queen):
-                qx = queens[y]
-                if x == qx or x + queen == qx + y or x - queen == qx - y:
-                    nextx = 1
-                    break
-            if nextx == 1:
-                nextx == 0
-                continue
-            if queen != size - 1:
-                queens[queen + 1] = 0
-                queens[queen] = x
-                queencalc(queen + 1)
-            else:
-                queens[queen] = x
-                printsolution(queens)
-    queencalc(0)
-
-if len(sys.argv) != 2:
-    print("Usage: nqueens N")
-    exit()
-try:
-    size = int(sys.argv[1])
-except (ValueError, TypeError):
-    print("N must be a number")
-    exit()
-nqueens(size)
+    # Solve the N Queens problem
+    queens = NQueens(n)
+    queens.solve()
