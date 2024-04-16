1f6005d27be31f82b03d19e448a96bee65ac0385
RuthT Sobande <136115703+RuthTSobande@users.noreply.github.com>
2024-04-17 00:36:27 +0100

    Update 11-main.py

--- 0x0B-python-input_output/11-main.py
+++ 0x0B-python-input_output/11-main.py
@@ -1,41 +1,42 @@
 #!/usr/bin/python3
-"""Defines a class Student."""
+import os
+import sys
+
+Student = __import__('11-student').Student
+read_file = __import__('0-read_file').read_file
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+path = sys.argv[1]
+
+if os.path.exists(path):
+    os.remove(path)
+
+student_1 = Student("John", "Doe", 23)
+j_student_1 = student_1.to_json()
+print("Initial student:")
+print(student_1)
+print(type(student_1))
+print(type(j_student_1))
+print("{} {} {}".format(student_1.first_name, student_1.last_name, student_1.age))
 
 
-class Student:
-    """Represent a student."""
+save_to_json_file(j_student_1, path)
+read_file(path)
+print("\nSaved to disk")
 
-    def __init__(self, first_name, last_name, age):
-        """Initialize a new Student.
 
-        Args:
-            first_name (str): The first name of the student.
-            last_name (str): The last name of the student.
-            age (int): The age of the student.
-        """
-        self.first_name = first_name
-        self.last_name = last_name
-        self.age = age
+print("Fake student:")
+new_student_1 = Student("Fake", "Fake", 89)
+print(new_student_1)
+print(type(new_student_1))
+print("{} {} {}".format(new_student_1.first_name, new_student_1.last_name, new_student_1.age))
 
-    def to_json(self, attrs=None):
-        """Get a dictionary representation of the Student.
 
-        If attrs is a list of strings, represents only those attributes
-        included in the list.
+print("Load dictionary from file:")
+new_j_student_1 = load_from_json_file(path)
 
-        Args:
-            attrs (list): (Optional) The attributes to represent.
-        """
-        if (type(attrs) == list and
-                all(type(ele) == str for ele in attrs)):
-            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
-        return self.__dict__
-
-    def reload_from_json(self, json):
-        """Replace all attributes of the Student.
-
-        Args:
-            json (dict): The key/value pairs to replace attributes with.
-        """
-        for k, v in json.items():
-            setattr(self, k, v)
+new_student_1.reload_from_json(j_student_1)
+print(new_student_1)
+print(type(new_student_1))
+print("{} {} {}".format(new_student_1.first_name, new_student_1.last_name, new_student_1.age))
