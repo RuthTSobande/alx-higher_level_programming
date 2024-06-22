e5546cbfe8c0b755d085732f30ff3f814c5bef67
RuthT Sobande <136115703+RuthTSobande@users.noreply.github.com>
2024-06-22 17:38:48 +0100

    Update 100-map.js

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
-const list = require('./100-data.js').list;
 
-let newList = list.map((x, i) => x * i);
+const list = require('./100-data').list;
+
 console.log(list);
-console.log(newList);
+console.log(list.map((x, i) => x * i));
