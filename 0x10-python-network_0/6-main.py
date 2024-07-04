049c1d1bd2895b4ae893f47e253bfeb95ae12576
RuthT Sobande <136115703+RuthTSobande@users.noreply.github.com>
2024-07-04 18:30:53 +0100

    Update 6-main.py

--- 0x10-python-network_0/6-main.py
+++ 0x10-python-network_0/6-main.py
@@ -1,17 +1,16 @@
 #!/usr/bin/python3
 # finds peak
 
-
-def search(lo, h, ints):
-    mid = (lo + h) // 2
-    if lo == h:
-        return ints[h]
-    if ints[mid] < ints[mid + 1]:
-        return(search(mid + 1, h, ints))
-    return(search(lo, mid, ints))
-
-
 def find_peak(list_of_integers):
+    def binary_search(nums, left, right):
+        if left == right:
+            return nums[left]
+        mid = (left + right) // 2
+        if nums[mid] < nums[mid + 1]:
+            return binary_search(nums, mid + 1, right)
+        else:
+            return binary_search(nums, left, mid)
+    
     if not list_of_integers:
-        return
-    return(search(0, len(list_of_integers) - 1, list_of_integers))
+        return None
+    return binary_search(list_of_integers, 0, len(list_of_integers) - 1)
