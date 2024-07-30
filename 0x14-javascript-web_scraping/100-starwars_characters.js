7e699359216a35db77a3e6a3795b3519c5ccd2c5
RuthT Sobande <136115703+RuthTSobande@users.noreply.github.com>
2024-07-30 23:36:08 +0100

    Update 100-starwars_characters.js

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,18 +1,15 @@
 #!/usr/bin/node
-
 const request = require('request');
-
-request('http://swapi.co/api/films/' + process.argv[2], function (err, res, body) {
-  if (err) console.log(err);
-  else {
-    let characters = JSON.parse(body).characters;
-    for (let character in characters) {
-      request(characters[character], function (err, res, body) {
-        if (err) console.log(err);
-        else {
+const url = 'https://swapi.co/api/films/' + process.argv[2];
+request(url, function (error, response, body) {
+  if (!error) {
+    const characters = JSON.parse(body).characters;
+    characters.forEach((character) => {
+      request(character, function (error, response, body) {
+        if (!error) {
           console.log(JSON.parse(body).name);
         }
       });
-    }
+    });
   }
 });
