db002f086ef0e7c8b3dac9c5919d500935e04944
RuthT Sobande <136115703+RuthTSobande@users.noreply.github.com>
2024-07-30 23:31:40 +0100

    Update 5-request_store.js

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,13 +1,14 @@
 #!/usr/bin/node
 
-const fs = require('fs');
 const request = require('request');
+const fs = require('fs');
+const url = process.argv[2];
+const filePath = process.argv[3];
 
-request.get(process.argv[2], function (err, res, data) {
-  if (err) console.log(err);
-  else {
-    fs.writeFile(process.argv[3], data, 'utf-8', (err, res) => {
-      if (err) console.log(err);
-    });
+request(url, function (err, response, body) {
+  if (err) {
+    console.log(err);
+  } else {
+    fs.writeFile(filePath, body, 'utf-8');
   }
 });
