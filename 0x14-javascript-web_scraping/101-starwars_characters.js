8668f9174ddce235159273b21d2d5ac9e3192234
RuthT Sobande <136115703+RuthTSobande@users.noreply.github.com>
2024-07-30 23:41:00 +0100

    Update 101-starwars_characters.js

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,27 +1,20 @@
 #!/usr/bin/node
-
 const request = require('request');
-
-request('http://swapi.co/api/films/' + process.argv[2], function (err, res, body) {
-  if (err) console.log(err);
-  else {
-    let characters = JSON.parse(body).characters;
-    let promiselist = [];
-    for (let character in characters) {
-      const namepromise = new Promise(resolve => {
-        request(characters[character], function (err, res, body) {
-          if (err) console.log(err);
-          else {
-            resolve(JSON.parse(body).name);
-          }
-        });
-      });
-      promiselist.push(namepromise);
-    }
-    Promise.all(promiselist).then(function (namelist) {
-      for (let name in namelist) {
-        console.log(namelist[name]);
-      }
-    });
+const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
+request(url, function (error, response, body) {
+  if (!error) {
+    const characters = JSON.parse(body).characters;
+    printCharacters(characters, 0);
   }
 });
+
+function printCharacters (characters, index) {
+  request(characters[index], function (error, response, body) {
+    if (!error) {
+      console.log(JSON.parse(body).name);
+      if (index + 1 < characters.length) {
+        printCharacters(characters, index + 1);
+      }
+    }
+  });
+}
