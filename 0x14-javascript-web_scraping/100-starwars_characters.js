f64d9aed82f61f5cf7c2bc2a39987e3c2f69c621
RuthT Sobande <136115703+RuthTSobande@users.noreply.github.com>
2024-07-30 23:38:26 +0100

    Update 100-starwars_characters.js

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,12 +1,27 @@
 #!/usr/bin/node
-const req = require('request');
-const url = 'https://swapi.co/api/films/' + process.argv[2];
-req(url, function (error, response, body) {
-  if (error) { console.log(error); }
-  JSON.parse(body).characters.forEach(function (peeps) {
-    req(peeps, function (error, response, body) {
-      if (error) { console.log(error); }
-      console.log(JSON.parse(body).name);
+const request = require('request');
+const film = process.argv[2];
+let url = 'http://swapi.co/api/people/';
+function filmcharacters (film, url) {
+    request(url, function (err, response, body) {
+	if (err) {
+	    console.log(err);
+	} else if (response.statusCode == 200) {
+	    let jsonobj = JSON.parse(body);
+	    let people = jsonobj.results;
+	    for (let i in people) {
+		for (let j in people[i].films) {
+		    if (people[i].films[j].includes(film)) {
+			console.log(people[i].name);
+		    }
+		}
+	    }
+	    if (jsonobj.next !== null) {
+		filmcharacters(film, jsonobj.next);
+	    }
+	} else {
+	    console.log('An error occured. Status code: ' + response.statusCode);
+	}
     });
-  });
-});
+}
+filmcharacters(film, url);
