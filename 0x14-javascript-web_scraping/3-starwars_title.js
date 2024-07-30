ff97b1a1708a8f68d9bde30b90eceb75021a5f11
RuthT Sobande <136115703+RuthTSobande@users.noreply.github.com>
2024-07-30 23:30:26 +0100

    Update 3-starwars_title.js

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,8 +1,16 @@
 #!/usr/bin/node
 
 const request = require('request');
+const episodeNum = process.argv[2];
+const API_URL = 'https://swapi-api.hbtn.io/api/films/';
 
-request('https://swapi.co/api/films/' + process.argv[2], function (err, res, body) {
-  if (err) console.log(err);
-  else console.log(JSON.parse(body).title);
+request(API_URL + episodeNum, function (err, response, body) {
+  if (err) {
+    console.log(err);
+  } else if (response.statusCode === 200) {
+    const responseJSON = JSON.parse(body);
+    console.log(responseJSON.title);
+  } else {
+    console.log('Error code: ' + response.statusCode);
+  }
 });
